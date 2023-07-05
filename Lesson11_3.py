4fc1b99cace60773ece0f926650d6ddde7b604cf
余友中 <qqlong2323@gmail.com>
2023-07-05 23:47:03 +0800

    modify

--- Lesson11_3.py
+++ Lesson11_3.py
@@ -4,3 +4,12 @@ group = int(input("請輸入大樂透組數: "))
 for _ in range(group):
     listlottery = list(tools.lottery())
     print(f"號碼為: {listlottery[:6]}\n特別號為:{listlottery[-1:]}")
+
+#請輸入本期大樂透電腦選號的組數:10
+import tools
+
+for n in range(int(input("請輸入本期大樂透電腦選號的組數:"))):
+    values = tools.lottery_poll()
+    print(f"您將中大獎的大樂透電腦選號第{n+1}組:{values[:6]}&特別號為:{values[-1:]}")
+print()
+print("!!!!!!!!!!!!!!!祝您中大獎!!!!!!!!!!!!!!!")
--- Practice/practice_09.py
+++ Practice/practice_09.py
@@ -4,7 +4,7 @@ import random
 
 def lottery(): 
     nums = set()
-    while(len(nums) <= 6):
+    while(len(nums) < 7):
         nums.add(random.randint(1, 49))
     return nums
 
