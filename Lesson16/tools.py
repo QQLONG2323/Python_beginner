8864941661d3d64b59bcabfd57d395cd69f23e76
余友中 <qqlong2323@gmail.com>
2023-07-12 00:39:56 +0800

    modify

--- Lesson16/tools.py
+++ Lesson16/tools.py
@@ -2,8 +2,7 @@ class Taiwan_AQI():
 
     API_KEY = "657f239f-f6e5-49fb-b8e0-966c8f789a5b"
 
-    @classmethod
-    
+    @classmethod    
     def download_AQI(cls) -> list:
         import requests
         response = requests.get(f"https://data.epa.gov.tw/api/v2/aqx_p_434?api_key={cls.API_KEY}&limit=1000&sort=monitordate desc&format=CSV")
