d83e75cde4eda819dea1766765f08ad2e1b769a6
余友中 <qqlong2323@gmail.com>
2023-07-14 09:17:02 +0800

    modify

--- Lesson/Lesson17/main.py
+++ Lesson/Lesson17/main.py
@@ -1,15 +1,16 @@
 from tools import Taiwan_AQI
 
 def get_best(dataList) -> list:
-    sorted_data_best = sorted(dataList, key = lambda a:a.aqi)
-    #for item in sorted_data_best:
-    #    print(item)      
-    return sorted_data_best[:3]
+    sorted_data_best = sorted(dataList, key = lambda a:a.aqi, reverse = True) #reverse = True 則排列順序由大到小, 如果都不設, 默認為False
+    for item in sorted_data_best:
+        print(item)      
+    #return sorted_data_best[:3]
         
 
 def get_bad(dataList) -> list:
-    sorted_data_bad = sorted(dataList, key = lambda a:a.aqi, reverse = True) #reverse = True 則排列順序由大到小, 如果都不設, 默認為False
-    return sorted_data_bad[:3]
+    pass
+    #sorted_data_bad = sorted(dataList, key = lambda a:a.aqi, reverse = True) #reverse = True 則排列順序由大到小, 如果都不設, 默認為False
+    #return sorted_data_bad[:3]
 
 def print_site_level(dataList) -> None:
     pass
@@ -23,14 +24,14 @@ def main():
         #print(str(err))
         return
     
-    #get_best(aqi_list)
+    get_best(aqi_list)
 
-    print("aqi目前站點最好的3個")
+    '''print("aqi目前站點最好的3個")
     for i in get_best(aqi_list):
         print(i)
     print("aqi目前站點最差的3個")
     for i in get_bad(aqi_list):
-        print(i)
+        print(i)'''
     
 
 
